478b782c279c6cce6ca1f0eced8e09f2a1c5aa6c
tbright92 <tylerbright2010@gmail.com>
2013-04-13 13:13:45 -0400

    Iteration 2 Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Blogger::Application.routes.draw do
   resources :articles
+
+  root to: 'articles#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
